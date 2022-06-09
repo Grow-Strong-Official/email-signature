a324110d3ad82be90b51d799f9c8739d27ed1493
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-06-09 10:14:57 -0600

    Updates

---
+++ precache-manifest.90e2c1b58341b57c6a8c9baf191e3c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "193706edf0228fefae56",
+    "url": "/email-signature/static/js/main.193706ed.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/email-signature/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "bae1d6adfff8dc099127",
+    "url": "/email-signature/static/css/2.ae445f0e.chunk.css"
+  },
+  {
+    "revision": "bae1d6adfff8dc099127",
+    "url": "/email-signature/static/js/2.bae1d6ad.chunk.js"
+  },
+  {
+    "revision": "3613c7522e4178f2bc95",
+    "url": "/email-signature/static/js/runtime~main.3613c752.js"
+  },
+  {
+    "revision": "522c7ac996d2324acb89dbf5ea9ff0db",
+    "url": "/email-signature/index.html"
+  }
+];
